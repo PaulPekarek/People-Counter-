b6222fb0d586fb6bf3b74d98a8069a1fa92826c1
Paul Pekarek <51837639+PaulPekarek@users.noreply.github.com>
2021-12-07 18:01:01 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -3,19 +3,19 @@ let countEl = document.getElementById("count-el")
 let count = 0
 
 //increases count 
-function increment() {
+increment = () => {
     count += 1;
     countEl.innerText = count;
 }
 
 //decreases count 
-function decrease() {
+decrease = () => {
     count = count - 1;
     countEl.textContent = count;
 }
 
 //saves count 
-function save () {
+save = () => {
     let countStr = " " + count + " ";
     saveEl.textContent += countStr;
     countEl.textContent = 0; 
